ab88dd2728af458ff264d096f3dc066156cc735d
Henrique Marcomini <henriquemarcomini@live.com>
2015-09-11 21:18:53 -0300

    Improve R³
    Improved the r³ collection

--- R3/Vetor.cs
+++ R3/Vetor.cs
@@ -19,7 +19,53 @@ namespace Metria.R3
 		}
 		#endregion
 		#region Construtores
+		public Vetor()
+		{
+			_z = 0;
+			_norma = 0;
+		}
+		public Vetor(float x, float y, float z)
+		{
+			_x = x;
+			_y = y;
+			_z = z;
+			_norma = 0;
+		}
+		public Vetor()
+		{
+			_z = 0;
+			_norma = 0;
+		}
+		public Vetor()
+		{
+			_z=0;
+			_norma = 0;
+		}
 
+
+		#endregion
+		#region Metodos
+
+		private float calculaNorma()
+		{
+			return (float)Math.Sqrt(Math.Pow(X,2)+Math.Pow(Y,2)+Math.Pow(Z,2));
+		}
+
+		public Vetor RetornaVersor()
+		{
+			return new Vetor(X/Norma,Y/Norma,Z/Norma);
+		}
+		
+		[Obsolete("Metodo ainda nao abastraido")]
+		public Vetor RetornaOrtogonal()
+		{
+			return null;
+		} 
+
+		public float ProdutoEscalar(Vetor V)
+		{
+			return X*V.X + Y*V.Y + Z*V.Z;
+		}
 
 
 		#endregion
